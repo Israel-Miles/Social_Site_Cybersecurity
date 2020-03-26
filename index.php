e6e22bca3ea9ad43ff5a9068f4dbe1e2c32b42a5
Israel Miles <ismi7509@colorado.edu>
2020-03-25 18:36:09 -0600

    add gitignore and index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+    $con = mysqli_connect("localhost", "root", "", "social");
+    
+    if(mysqli_connect_errno()) {
+        echo "Failed to connect: " . mysqli_connect_errno();
+    }
+
+    $query = mysqli_query($con, "INSERT INTO test VALUES(NULL, 'Izzy')");
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Hello Izzy!
+</body>
+</html>
