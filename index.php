fc241b4afe97464dab1d0d5394a1d1c0ee152b5d
Israel-Miles <izzyjmiles@gmail.com>
2020-03-26 08:03:36 -0600

    redirect users not logged in

--- index.php
+++ index.php
@@ -1,20 +1,9 @@
 <?php
-    require 'config/config.php';
-    require 'includes/form_handlers/register_handler.php';
-    require 'includes/form_handlers/login_handler.php';
-
-    $query = mysqli_query($con, "INSERT INTO test VALUES(NULL, 'Izzy')");
+    include("includes/header.php");
+    session_destroy();
 ?>
 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
     Hello Izzy!
 </body>
 </html>
