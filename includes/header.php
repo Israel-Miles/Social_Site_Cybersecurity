6d658af2cc2517b5e2ab8fa835e4a10bf7697f50
Israel-Miles <izzyjmiles@gmail.com>
2020-03-26 09:41:04 -0600

    add nav bar

--- includes/header.php
+++ includes/header.php
@@ -3,6 +3,8 @@
 
     if (isset($_SESSION['username'])) {
         $user_logged_in = $_SESSION['username'];
+        $user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$user_logged_in'");
+        $user = mysqli_fetch_array($user_details_query);
     }
     else {
         header("Location: register.php");
@@ -15,7 +17,41 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Welcome to Coronagram</title>
+
+    <!-- JavaScript -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.js"></script>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 <body>
+    <div class="top_bar">
+        <div class="logo">
+            <a href="index.php">Coronagram</a>
+        </div>
+
+        <nav>
+            <a href="#">
+                <?php echo $user['first_name']; ?>
+            </a>
+            <a href="index.php">
+                <i class="fa fa-home fa-lg"></i>
+            </a>
+            <a href="#">
+                <i class="fa fa-envelope fa-lg"></i>
+            </a>
+            <a href="#">
+                <i class="fa fa-bell-o fa-lg"></i>
+            </a>
+            <a href="#">
+                <i class="fa fa-users fa-lg"></i>
+            </a>
+            <a href="#">
+                <i class="fa fa-cog fa-lg"></i>
+            </a>
+        </nav>
+    </div>
