96a7f392bce94cf5923d457f95c38dd183a3f33e
Israel Miles <ismi7509@colorado.edu>
2020-03-25 20:27:24 -0600

    variable validation

--- register.php
+++ register.php
@@ -61,6 +61,27 @@
         else {
             echo "Emails don't match";
         }
+
+        if(strlen($first_name) > 25 || strlen($first_name) < 2) {
+            echo "Your first name must be between 2 and 25 characters";
+        }
+
+        if(strlen($last_name) > 25 || strlen($last_name) < 2) {
+            echo "Your last name must be between 2 and 25 characters";
+        }
+
+        if($password != $password2) {
+            echo "Your passwords do not match";
+        }
+        else {
+            if(preg_match('/[A-Za-z0-9]/', $password)) {
+                echo "Your password can only contain english characters or numbers";
+            }
+        }
+
+        if(strlen($password) > 30 || strlen($password) < 5) {
+            echo "Your password must be between 5 and 30 characters";
+        }
     }
 ?>
 
