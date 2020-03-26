1894cf468513a3584825daf439f907632a8868d5
Israel-Miles <izzyjmiles@gmail.com>
2020-03-26 06:24:38 -0600

    starter css for register

--- register.php
+++ register.php
@@ -11,6 +11,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome to Coronagram!</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/register_style.css">
 </head>
 <body>
     <form action="register.php" method="POST">
