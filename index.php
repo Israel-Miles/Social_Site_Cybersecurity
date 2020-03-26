6c2c4150b460dc13bb774320b33ba89b1d9f4ea8
Israel-Miles <izzyjmiles@gmail.com>
2020-03-26 10:21:27 -0600

    start styling for user details

--- index.php
+++ index.php
@@ -2,8 +2,9 @@
     include("includes/header.php");
     session_destroy();
 ?>
-
-
-    Hello Izzy!
+        <div class="user_details column">
+            <a href="#"><img src="<?php echo $user['profile_pic']; ?>" alt=""></a>
+        </div>
+    </div>
 </body>
 </html>
